14f9b4d26b144f38b85529e34b7482ed8bb4b16f
volausson <veronicagolausson@gmail.com>
2022-10-03 19:24:33 +0000

    add create function to LikeSerializer to handle dupllicate likes

--- comments/views.py
+++ comments/views.py
@@ -1,13 +1,15 @@
 from rest_framework import generics, permissions
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Comment
-from .serializers import CommentSerializer
+from .serializers import CommentSerializer, CommentDetailSerializer
 
 
 class CommentList(generics.ListCreateAPIView):
-
-    permission_classes = [permission.IsAuthenticatedOrReadOnly]
+    """
+    List comments or create a comment if logged in.
+    """
     serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Comment.objects.all()
 
     def perform_create(self, serializer):
@@ -15,7 +17,9 @@ class CommentList(generics.ListCreateAPIView):
 
 
 class CommentDetail(generics.RetrieveUpdateDestroyAPIView):
-
+    """
+    Retrieve a comment, or update or delete it by id if you own it.
+    """
     permission_classes = [IsOwnerOrReadOnly]
     serializer_class = CommentDetailSerializer
     queryset = Comment.objects.all()
