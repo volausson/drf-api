2c9aafbf92a0eb9ac335069ee80d51fecc24607c
volausson <veronicagolausson@gmail.com>
2022-10-04 14:29:31 +0000

    add extra fields to Posts and Profiles

--- followers/views.py
+++ followers/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from drf_api.permissions import IsOwnerOrReadOnly
+from likes.models import Follower
+from likes.serializers import FollowerSerializer
 
-# Create your views here.
+
+class FollowerList(generics.ListCreateAPIView):
+
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class FollowerDetail(generics.RetrieveDestroyAPIView):
+
+    permission_classes = [IsOwnerOrReadOnly]
+    serializer_class = FollowerSerializer
+    queryset = Follower.objects.all()
