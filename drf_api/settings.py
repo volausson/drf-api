d0f69082460c774f2174885a4f997db610cdd6f4
volausson <veronicagolausson@gmail.com>
2022-10-05 11:19:58 +0000

    correct minor problems

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -86,6 +86,25 @@ INSTALLED_APPS = [
     'followers',
 ]
 SITE_ID = 1
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [(
+        'rest_framework.authentication.SessionAuthentication'
+        if 'DEV' in os.environ
+        else
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
+    )],
+    'DEFAULT_PAGINATION_CLASS':
+
+    }
+REST_USE_JWT = True
+JWT_AUTH_COOKIE = 'my-app-auth'
+JWT_AUTH_SECURE = True
+JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
+REST_AUTH_SERIALIZERS = {
+    'USER_DETAILS_SERIALIZER':
+    'drf_api.serializers.CurrentUserSerializer'
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
